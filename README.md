21e18eeef84eb2e0711b120e4536cc574204de7d
Andreas Heigl <andreas@heigl.org>
2018-05-06 10:35:09 +0200

    Add first working commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# password-valueobject
-A ValueObject for passwords - Stop leaking passwords to logs or stacktraces!
+# org_heigl/password
+
+An Object for passwords - Stop leaking passwords to logs or stacktraces!
+
+## Wyh?
+
+The discussions that spun up around twitter leaking passwords to logfiles left me thinking.
+
+It can actually happen quite easily to have passwords come up into log-files when
+you put stack-traces into logs. And that brought me to thinking how to avoid that
+accidentally. The answer to me is a vaule-object with a bit of logic that handles the password but
+won't accidentaly leak it.
+
+## Installation
+
+THis is best installed using composer like this:
+
+```bash
+composer require org_heigl/password
+```
+
+## Usage
+
+Instead of passing the password as a string create a Password-Object and pass that.
+
+```php
+$password = Password::createFromPlainText($request->getParam('password'));
+```
+
+Verifying the password is done right within the Object like this:
+
+```php
+$password->matchesHash($hashFromPhpPasswordHashingApi);
+```
+
+Additionally you can get a new hash for the password like this:
+
+```php
+$hash = $password->getNewHash();
+```
+
+And to wrap up the API of PHPs password-hashing API there's also a method to check
+whether the password should be rehashed
+
+```php
+$password->shouldBeRehashed();
+```
+
+If you **really** need to get the plaintext password the password-object was initialized with
+(f.e. for use with ```ldap_bind```) you can do that as well:
+
+```php
+$plaintextPassword = $password->getPlainTextPasswordAndYesIKnowWhatIAmDoingHere();
+```
+
+
---
+++ src/Password.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) Andreas Heigl<andreas@heigl.org> All rights reserved.
+ *
+ * Licensed under the MIT License. See LICENSE.md file in the
+ * project root for full license information.
+ */
+
+namespace Org_Heigl\Password;
+
+class Password
+{
+    private $password;
+
+    private $hash;
+
+    private function __construct(string $password)
+    {
+        $this->password = $password;
+        $this->hash = null;
+    }
+
+    public static function createFromPlainText(string $password) : self
+    {
+        return new self($password);
+    }
+
+    public function __toString()
+    {
+        return '*******';
+    }
+
+    public function matchesHash(string $hash) : bool
+    {
+        $this->hash = $hash;
+
+        return password_verify($this->password, $this->hash);
+    }
+
+    public function shouldBeRehashed(int $algorithm = PASSWORD_DEFAULT, array $options = []) : bool
+    {
+        if (null === $this->hash) {
+            return true;
+        }
+
+        return password_needs_rehash($this->hash, $algorithm, $options);
+    }
+
+    public function getNewHash(int $algorithm = PASSWORD_DEFAULT, array $options = []) : string
+    {
+        return password_hash($this->password, $algorithm, $options);
+    }
+
+    public function getPlainTextPasswordAndYesIKnowWhatIAmDoingHere() : string
+    {
+        trigger_error(
+            'Password was leaked in clear text using the "Password::getPlainTextPasswordAndYesIKnowWhatIAmDoingHere"-function!!',
+            E_USER_WARNING
+        );
+
+        return $this->password;
+    }
+}
